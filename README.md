69cef54af789da23ffc135f4453b36c40ee5e7a6
Dan Kelley <kelley.dan@gmail.com>
2014-12-11 19:26:40 -0400

    gsw_sa_from_sp start work

--- README.md
+++ README.md
@@ -4,3 +4,14 @@ teos10
 R package for thermodynamic equation of seawater.  The main function, named
 ``teos10``, is a simple interface to the C library as of TEOS version 3.03,
 downloaded from [teos-10.org](http://www.teos-10.org) on 2014-08-25.
+
+NOT COMPLETE YET!
+
+TO DO: 
+
+1. rewrite SP to SA conversion with closures; mimic for other functions
+
+2. document with oxygen
+
+3. get to build on windows
+
--- tests/teos10.R
+++ tests/teos10.R
@@ -1,8 +1,18 @@
 library(teos10)
-SA <- teos10("gsw_sa_from_sp",
-             c(34.5487,34.7275,34.8605,34.6810,34.5680,34.56),
-             c(10,50,125,250,600,1000),rep(188,6),rep(4,6))
+
+# URLs indicate the sources for test values (matlab tests).
+ 
+# http://www.teos-10.org/pubs/gsw/html/gsw_SA_from_SP.html
+SP <- c(34.5487,34.7275,34.8605,34.6810,34.5680,34.56)
+p <- c(10,50,125,250,600,1000)
+longitude <- rep(188,6)
+latitude <- rep(4,6)
+SA <- teos10("gsw_sa_from_sp", SP, p, longitude, latitude)
 SAexpected <- c(34.711778344814114, 34.891522618230098, 35.025544862476920,
                 34.847229026189588, 34.736628474576051, 34.732363065590846)
+stopifnot(all.equal(SAexpected, SA))
+SA <- gsw_SA_from_SP(SP, p, longitude, latitude)
+stopifnot(all.equal(SAexpected, SA))
+SA <- gsw_SA_from_SP(SP, p, longitude[1], latitude[1])
 stopifnot(all.equal(SAexpected, SA))
 
