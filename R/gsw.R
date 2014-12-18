b42f34efd46b32aa827b970377a78acf600d8ceb
Dan Kelley <kelley.dan@gmail.com>
2014-12-18 08:47:37 -0400

    reshape args to match first

--- R/gsw.R
+++ R/gsw.R
@@ -1,16 +1,45 @@
+#' Reshape list elements to match the shape of the first element.
+#'
+#' @param l A list of elements, typically arguments that will be used in GSW functions.
+#' @return A list with all elements of same shape (length or dimension).
+argfix <- function(l)
+{
+    n <- length(l)
+    if (n > 0) {
+        length1 <- length(l[[1]])
+        for (i in 2:n) {
+            if (length(l[[i]]) != length1) {
+                l[[i]] <- rep(l[[i]], length.out=length1)
+            }
+        }
+        if (is.matrix(l[[1]])) {
+            for (i in 2:n) {
+                dim(l[[i]]) <- dim(l[[1]])
+            }
+        }
+    }
+    l
+}
+
 #' Convert from temperature to conservative temperature
 #' 
 #' @param SA Absolute salinity
-#' @param t In-situ temperature in degrees C.
-#' @param p Pressure in decibars. FIXME: check unit
+#' @param t In-situ temperature (ITS-90) in degrees C.
+#' @param p Sea pressure in decibars.
 #' @examples 
 #' gsw_CT_from_t(34.7118, 28.7856, 10) # 28.809919826700281
 #' @references
 #' \url{http://www.teos-10.org/pubs/gsw/html/gsw_CT_from_t.html}
 gsw_CT_from_t <- function(SA, t, p)
 {
-    .C("wrap_gsw_ct_from_t", SA=as.double(SA), t=as.double(t), p=as.double(p),
-       n=length(SA), rval=double(length(SA)))$rval
+    l <- argfix(list(SA=SA, t=t, p=p))
+    n <- length(l[[1]])
+    rval <- .C("wrap_gsw_ct_from_t",
+               SA=as.double(l$SA), t=as.double(l$t), p=as.double(l$p),
+               n=n, rval=double(n))$rval
+    if (is.matrix(SA))
+        dim(rval) <- dim(SA)
+    rval
 }
 
 #' Convert from practical salinity to absolute salinity
@@ -26,35 +55,21 @@ gsw_CT_from_t <- function(SA, t, p)
 #' \url{http://www.teos-10.org/pubs/gsw/html/gsw_SA_from_SP.html}
 gsw_SA_from_SP <- function(SP, p, longitude, latitude)
 {
-    ## old ## FIXME: probably should do all but second two lines in some general
-    ## old ## way, either with a function call or a closure.
-    ## old if (missing(SP)) stop("must supply SP")
-    ## old if (missing(p)) stop("must supply p")
-    ## old if (missing(longitude)) stop("must supply longitude")
-    ## old if (missing(latitude)) stop("must supply latitude")
-    ## old SP <- as.matrix(SP)
-    ## old p <- as.matrix(p)
-    ## old SPdim <- dim(SP)
-    ## old if (!all.equal(SPdim, dim(p))) stop("dim(SP) and dim(p) must match")
-    ## old n <- prod(SPdim)
-    ## old if (is.vector(longitude)) longitude <- matrix(longitude, nrow=SPdim[1], ncol=SPdim[2])
-    ## old else if (SPdim != dim(longitude)) stop("dim(sp) and dim(longitude) must match")
-    ## old if (is.vector(latitude)) latitude <- matrix(latitude, nrow=SPdim[1], ncol=SPdim[2])
-    ## old else if (SPdim != dim(latitude)) stop("dim(sp) and dim(latitude) must match")
-    ## old if (is.vector(latitude)) latitude <- matrix(latitude, nrow=SPdim[1], ncol=SPdim[2])
-    ## old .C("wrap_gsw_sa_from_sp", SP=as.double(SP), p=as.double(p), 
-    ## old    longitude=as.double(longitude), latitude=as.double(latitude),
-    ## old    n=as.integer(n), rval=double(n))$rval
-    .C("wrap_gsw_sa_from_sp", SA=as.double(SP), p=as.double(p),
-       longitude=as.double(longitude), latitude=as.double(latitude),
-       n=length(SP), rval=double(length(SP)))$rval
+    l <- argfix(list(SP=SP, p=p, longitude=longitude, latitude=latitude))
+    n <- length(l[[1]])
+    rval <- .C("wrap_gsw_sa_from_sp",
+               SA=as.double(l$SP), p=as.double(l$p), longitude=as.double(l$longitude), latitude=as.double(l$latitude),
+               n=n, rval=double(n))$rval
+    if (is.matrix(SP))
+        dim(rval) <- dim(SA)
+    rval
 }
 
 #' Convert from conductivity to practical salinity
 #' 
-#' @param C Conductivity ratio. FIXME: check unit
-#' @param t In-situ temperature in degrees C.
-#' @param p Pressure in decibars. FIXME: check unit
+#' @param C Conductivity in mS/cm.
+#' @param t In-situ temperature (ITS-90) in degrees C.
+#' @param p Sea pressure in decibars.
 #' @return Practical salinity.
 #' @examples 
 #' gsw_SP_from_C(34.5487, 28.7856, 10) # 20.009869599086951
@@ -62,8 +77,14 @@ gsw_SA_from_SP <- function(SP, p, longitude, latitude)
 #' \url{http://www.teos-10.org/pubs/gsw/html/gsw_SP_from_C.html}
 gsw_SP_from_C <- function(C, t, p)
 {
-    .C("wrap_gsw_sp_from_c", C=as.double(C), t=as.double(t), p=as.double(p),
-       n=length(C), rval=double(length(C)))$rval
+    l <- argfix(list(C=C, t=t, p=p))
+    n <- length(l[[1]])
+    rval <- .C("wrap_gsw_sp_from_c",
+               C=as.double(l$C), t=as.double(l$t), p=as.double(l$p),
+               n=n, rval=double(n))$rval
+    if (is.matrix(C))
+        dim(rval) <- dim(C)
+    rval
 }
 
 
